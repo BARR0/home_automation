f2d449d9234ef71286e315809ba4fef8afe97afb
Andres Barro <andresbarroe@gmail.com>
2019-11-28 17:40:29 -0600

    Timer test
    Signed-off-by: Andres Barro <andresbarroe@gmail.com>

--- Sources/timer.c
+++ Sources/timer.c
@@ -7,6 +7,7 @@
 
 #include "derivative.h"
 #include "common.h"
+#include "timer.h"
 
 void timerInit(void)
 {
@@ -18,7 +19,29 @@ void timerInit(void)
     TPM0->SC |= 0x02;                                                         //Set TPM0 prescaler to be of 1MHz
     TPM0->SC &= ~TPM_SC_CMOD(1);                                              //Stop timer TPM0
 
+    SIM->SOPT2 |= SIM_SOPT2_CLKOUTSEL(4);
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+    PORTC->PCR[3] |= PORT_PCR_MUX(5);
+
     SIM->SCGC6 |= SIM_SCGC6_RTC_MASK;
+    SIM->SOPT1 &= ~SIM_SOPT1_OSC32KSEL_MASK;
+    SIM->SOPT1 |= SIM_SOPT1_OSC32KSEL(0);
+    // RTC->LR |= RTC_LR_SRL_MASK | RTC_LR_CRL_MASK;
+    RTC->CR |= RTC_CR_SWR_MASK;
+    RTC->CR &= ~RTC_CR_SWR_MASK;
+    // RTC->TAR = 0xFFFFFFFF;
+    RTC->CR |= RTC_CR_OSCE_MASK;
+    timerSleep1Second();
+    RTC->SR &= ~RTC_SR_TCE_MASK;
+    RTC->SR &= ~RTC_SR_TIF_MASK;
+    RTC->SR &= ~RTC_SR_TOF_MASK;
+    RTC->TSR = 0xFF;
+    RTC->SR |= RTC_SR_TCE_MASK;
+
+    // RTC->SR |= RTC_SR_TIF_MASK;
+    // RTC->SR |= RTC_SR_TOF_MASK;
+    // RTC->TSR = 0xFF;
+    // RTC->SR |= RTC_SR_TCE_MASK;
 }
 
 int timerGetRTC()
@@ -29,7 +52,11 @@ int timerGetRTC()
 
 void timerSetRTC(int time)
 {
-    // TODO
+    RTC->SR &= ~RTC_SR_TCE_MASK;
+    RTC->SR &= ~RTC_SR_TIF_MASK;
+    RTC->SR &= ~RTC_SR_TOF_MASK;
+    RTC->TSR = time;
+    RTC->SR |= RTC_SR_TCE_MASK;
 }
 
 void timerSleep1Second(void)
@@ -38,7 +65,7 @@ void timerSleep1Second(void)
     TPM0->CNT = 0;               //Set TPM0 counter to 0
     TPM0->SC |= TPM_SC_TOF_MASK; //Clear TPM0 Timer Overflow Flag
     TPM0->SC |= TPM_SC_CMOD(1);  //Start TPM0
-    TPM0->MOD = 1000; //Set TPM0 Modulo to overflow every 1ms
+    TPM0->MOD = 1000;            //Set TPM0 Modulo to overflow every 1ms
     int i;
     for (i = 0; i < 1000; i++)
     {
